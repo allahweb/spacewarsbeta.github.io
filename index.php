9b9339ad872a5bb35f62cb14bcf3b5f4732e01e5
allahweb <110897795+allahweb@users.noreply.github.com>
2022-08-09 09:26:48 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,155 @@
+<html lang="en">
+
+
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" href="spacewars.html" type="image/x-icon">
+<link rel="apple-touch-icon" sizes="180x180" href="steam/apps/1558930/header055c.jpg?t=1646147748">
+<link rel="icon" type="image/png" sizes="32x32" href="steam/apps/1558930/header055c.jpg?t=1646147748">
+<link rel="icon" type="image/png" sizes="16x16" href="steam/apps/1558930/header055c.jpg?t=1646147748">
+<link rel="manifest" href="assets/images/favicon/site.html">
+<link rel="mask-icon" href="assets/images/favicon/safari-pinned-tab.html" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta content="Space Wars" property="og:title">
+<meta content="Created by master storytellers The Chinese Room, the complete version of this popular multi-award winning Apple Arcade game has been remastered for the big screen and includes all the bonus content from the original. A HERO WILL GO!
+" property="og:description">
+<meta content="#ffffff" data-react-helmet="true" name="theme-color">
+<meta content='https://cdn.cloudflare.steamstatic.com/steam/apps/1558930/header.jpg?t=1646147748' property='og:image'>
+<meta property="og:image">
+<title>Space Wars</title>
+<link rel="stylesheet" href="css/rawline.css">
+<link rel="stylesheet" href="css2b9d3?family=Raleway:wght@300;400;500;600;700;800&amp;display=swap">
+<link rel="stylesheet" href="css2-180d1?family=Bebas+Neue&amp;family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap">
+<link rel="stylesheet" href="ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+<link rel="stylesheet" href="assets/reset.html">
+<link rel="stylesheet" href="assets/index.css">
+</head>
+<body>
+<div class="wrapper">
+<nav class="navbar">
+<div class="navbar__container">
+<div class="navbar__branding">
+<a href="index.php">
+<svg id="logo" width="150" height="150" viewbox="0 0 130 130" xmlns="../cdn.cloudflare.steamstatic.com/steam/apps/1558930/header055c.jpg?t=1646147748">
+</svg>
+<span>Space Wars</span>
+</a>
+</div>
+<div class="navbar__items navbar__items--mobile">
+<a href="#" class="navbar__item">
+<label for="input-navbar">
+<i class="fas fa-bars"></i>
+</label>
+</a>
+</div>
+<input type="checkbox" id="input-navbar" style="display: none;">
+<div class="navbar__items">
+<a href="/SetupSpaceWars.zip" class="navbar__item navbar__item--highlighted">Download</a>
+</div>
+</div>
+</nav>
+<main class="main">
+<div class="main__container">
+<section class="section section--hero">
+<div class="clouds"></div>
+<div class="section__container">
+<div class="section__content">
+<h1 class="section__heading">Space Wars</h1>
+<p class="section__subheading">The new best 2D Arcade Game...</p>
+<div class="section__buttons">
+<a href="#section-about" class="button button--secondary">Learn More!</a>
+<a href="/SetupSpaceWars.zip" class="button button--primary">Download Now!</a>
+</div>
+</div>
+</div>
+</section>
+<div>
+<section class="section section--about" id="section-about">
+<div class="section__container">
+<div class="section__content">
+<h2 class="section__heading">About Space Wars</h2>
+<p class="section__description">
+    Created by master storytellers The Chinese Room, the complete version of this popular multi-award winning Apple Arcade game has been remastered for the big screen and includes all the bonus content from the original. A HERO WILL GO!
+</p>
+</div>
+</div>
+<section class="section section--feature">
+<div class="section__container">
+    <p class="section__description">
+        <p style="font-weight: bold;"></p>
+          
+
+        </div>
+        <div class="section__content">
+        <h2 class="section__heading">
+        
+        </h2></div>
+        </div> 
+        </section>
+        <section class="section section--feature">
+        <div class="section__container">
+        <p style="font-weight: bold;"></p>
+        <p>
+        <p>
+        <div class="section__image" style="--image-url: url(steam/apps/1558930/ss_2809b58fb07da7cced5e7266e21dfbe67902ae74.1920x1080.jpg);">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/9S4dU30M7ZI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <div class="section__content">
+        <h2 class="section__heading"></h2>
+        <p class="section__description">
+        <a href="https://store.tecknix.com/"></a>
+        </p></div>
+        </div>
+        </section>
+        <section class="section section--feature">
+        <div class="section__container">
+        <p style="font-weight: bold;"></p>
+        <p>
+        <p>
+<div class="section__content">
+<h2 class="section__heading"></h2>
+<p class="section__description">
+</p>
+<p style="color:red; font-size:24px; font-weight:bold;">SYSTEM REQUIREMENTS
+<p style="color:gray; font-size:19px; font-weight:bold;">MINIMUM:
+<p>OS: Windows Vista, 7, 8, or 10
+<p>Processor: Intel Dual-Core Duo 1.6GHz or faster processor
+<p>Memory: 512 MB RAM
+<p>Graphics: Integrated
+<p>Storage: 100 MB available space
+</p></div></div></section>
+<section class="section section--download" id="section-download">
+<div class="section__container">
+<div class="section__content">
+<h2 class="section__heading">Download Now</h2>
+<p class="section__description">
+By downloading you agree to our <a href="" target="_self">EULA</a> and <a href="https://spacewars-beta.tk/privacypolicy.php" target="_self">Privacy Policy</a>
+</p>
+<div class="section__buttons">
+<a href="/SetupSpaceWars.zip" class="button button--primary">Download</a>
+</div>
+</div>
+</div>
+</section>
+</section></div>
+</div>
+</main>
+<footer class="footer" id="footer">
+<div class="footer__container">
+<div class="footer__content">
+<div class="footer__copyright">
+</div>
+<div class="footer__affiliation">
+</div>
+</div>
+</div>
+</footer>
+</div>
+
+
+</body>
+
+</html>
